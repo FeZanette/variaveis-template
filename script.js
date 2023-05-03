514ab0d75b9050c78d617d18a1db91da3b47ec4f
FeZanette <fernandazanette1975@gmail.com>
2023-05-02 21:36:22 -0300

    exercicios da aula variaveis

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+//let nome = prompt ("Digite seu nome")
 
+//let idade = Number(prompt("Digite sua idade"))
+
+//let presenca = confirm ("Confirme sua presenca")
+
+//console.log (typeof nome)
+
+//console.log (typeof idade)
+
+//console.log (typeof presenca)
+
+//console.log ("Estudante", nome, "tem", idade, "anos. Presença", presenca)
+
+console.log ("nome:", typeof nome, ", idade:", typeof idade, ", presenca:", typeof presenca)
+
+
+const nomeDoLivro = prompt ("Digite o nome do livro")
+
+let numeroDePaginas = Number (prompt("Digite o numero de paginas"))
+
+let confirmarLeitura = confirm ("Confirme se voce ja leu")
+
+console.log (typeof nomeDoLivro)
+
+console.log (typeof numeroDePaginas)
+
+console.log (confirmarLeitura)
+
+//console.log ("O livro", nomeDoLivro, "tem", numeroDePaginas, "páginas. Eu já o li", confirmarLeitura, ", confirmarLeitura:", typeof confirmarLeitura)
